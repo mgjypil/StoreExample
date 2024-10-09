becf60d27587887248ca033df6835c2b7bd70185
Maxine <mgjypil@gmail.com>
2024-10-08 19:49:25 -0700

    changed var names + imgs

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,38 @@
-const modal = document.querySelectorAll('.preview');
+const preview = document.querySelectorAll('.preview');
 const cardBtn = document.querySelectorAll('.product');
-const modalClose = document.querySelectorAll('.preview-close');
-const modalCard = document.querySelectorAll('.preview');
+const previewClose = document.querySelectorAll('.preview-close');
+const previewCard = document.querySelectorAll('.preview');
 
-let activeModal = (modalClick) => {
-    modal[modalClick].classList.add('active-preview');
+let activePreview = (previewClick) => {
+    preview[previewClick].classList.add('active-preview');
 }
 
 // show preview
 cardBtn.forEach((cardBtn, i) => {
     cardBtn.addEventListener('click', () => {
-        activeModal(i)
+        activePreview(i)
     })
 })
 
 // hide preview
-modalClose.forEach((modalClose) => {
-    modalClose.addEventListener('click', () => {
-        modal.forEach((modalRemove) => {
-            modalRemove.classList.remove('active-preview')
+previewClose.forEach((previewClose) => {
+    previewClose.addEventListener('click', () => {
+        preview.forEach((previewRemove) => {
+            previewRemove.classList.remove('active-preview')
         })
     })
 })
 
 // hide preview when clicking background
-modal.forEach((modal) => {
-    modal.addEventListener('click', () => {
-        modal.classList.remove('active-preview')
+preview.forEach((preview) => {
+    preview.addEventListener('click', () => {
+        preview.classList.remove('active-preview')
     })
 })
 
 // dont hide preview on product click by event propagation
-modalCard.forEach((modalCard) => {
-    modalCard.addEventListener('click', (e) => {
+previewCard.forEach((previewCard) => {
+    previewCard.addEventListener('click', (e) => {
         e.stopPropagation()
     })
 })
